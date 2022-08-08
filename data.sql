3f7eccca2be5828881d22bd8b1de67b9749ebf77
lomantech <87063237+lomantech@users.noreply.github.com>
2022-08-08 16:07:20 +0300

    Add data to animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,4 @@
-/* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals (name, date_of_birth, weight_kg, neutered, escape_attemps) values('Agumon', '2020-02-03', 10.23, True, 0);
+insert into animals (name, date_of_birth, weight_kg, neutered, escape_attemps) values('Gabumon', '2018-11-15', 8, True,2);
+insert into animals (name, date_of_birth, weight_kg, neutered, escape_attemps) values('Pikachu', '2021-01-07', 15.04, False,1);
+insert into animals (name, date_of_birth, weight_kg, neutered, escape_attemps) values('Devimon', '2017-05-12', 11, True,5);
